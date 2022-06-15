2a1acc149f9de8af1bc7336e0fb1c9ccfa7f7ece
guslopezr <guslopezr@gmail.com>
2022-06-15 15:55:44 -0400

    solución al problema de la suma y función

--- assets/js/index2.js
+++ assets/js/index2.js
@@ -21,19 +21,30 @@ totalcards = (op1 + op2 + op3);
 console.log(totalcards);
 */
 
+/*
 let opcionUno = document.querySelector('#selecc1').value;
 console.log('este es el valor de opcionUno ', opcionUno);
 let opcionDos = document.querySelector('#selecc2').value;
 let opcionTres = document.querySelector('#selecc3').value;
-let totalCards = opcionUno + opcionDos + opcionTres;
+let totalCards = Number(opcionUno) + Number(opcionDos) + Number(opcionTres);
 let texto = '';
 console.log('esta es la suma de los cards', totalCards)
+*/
+
 
 function validarCantidad() {
+    let opcionUno = document.querySelector('#selecc1').value;
+    console.log('este es el valor de opcionUno ', opcionUno);
+    let opcionDos = document.querySelector('#selecc2').value;
+    let opcionTres = document.querySelector('#selecc3').value;
+    let totalCards = Number(opcionUno) + Number(opcionDos) + Number(opcionTres);
+    let texto = '';
+    console.log('esta es la suma de los cards', totalCards)
+
     if (totalCards >= 10) {
         texto = 'Puedes elegir hasta 10 stickers';
     } else {
-        texto = 'Sigue agregando';
+        texto = '¡Sigue agregando, aún puedes elegir más stickers!';
     }
     document.getElementById("verificar").innerHTML = texto;
 }
