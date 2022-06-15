ac7a5c9a8fb763099d464c4858840f8ff81cb169
guslopezr <guslopezr@gmail.com>
2022-06-15 13:35:38 -0400

    primer commit

---
+++ assets/js/index2.js
@@ -0,0 +1,39 @@
+/*
+
+// store input numbers
+let num1 = document.getElementById("input1");
+let sum1 = document.getElementById("input1");
+const num2 = document.getElementById("input2").value;
+const num3 = document.getElementById("input3").value;
+
+//add two numbers
+const sum = num1 + num2 + num3;
+
+// display the sum
+console.log(sum);
+
+*/
+/*
+var op1 = document.querySelector('#input1').value;
+var op2 = document.querySelector('#input2').value;
+var op3 = document.querySelector('#input3').value;
+totalcards = (op1 + op2 + op3);
+console.log(totalcards);
+*/
+
+let opcionUno = document.querySelector('#selecc1').value;
+console.log('este es el valor de opcionUno ', opcionUno);
+let opcionDos = document.querySelector('#selecc2').value;
+let opcionTres = document.querySelector('#selecc3').value;
+let totalCards = opcionUno + opcionDos + opcionTres;
+let texto = '';
+console.log('esta es la suma de los cards', totalCards)
+
+function validarCantidad() {
+    if (totalCards >= 10) {
+        texto = 'Puedes elegir hasta 10 stickers';
+    } else {
+        texto = 'Sigue agregando';
+    }
+    document.getElementById("verificar").innerHTML = texto;
+}
